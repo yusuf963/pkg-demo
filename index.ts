074774682b6c53b4c87fb07082040b1c195be11d
yusuf963 <mohammad963yusuf@gmail.com>
2023-09-03 12:49:05 +0100

    create new function

--- index.ts
+++ index.ts
@@ -3,5 +3,9 @@ export const add = (a: number, b: number) => {
 };
 
 const deduct = (a: number, b: number): number => {
-  return a - b;
+  return a - b + 0;
+};
+
+const multiply = (a: number, b: number): number => {
+  return a * b;
 };
